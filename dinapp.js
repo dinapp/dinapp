5117803c124a11f25a3f241d38e05bb07a319845
Gustavo Miranda <gustavo.miranda@r2t.company>
2019-11-07 13:01:34 -0300

    konami code

--- dinapp.js
+++ dinapp.js
@@ -23,6 +23,8 @@ function bencaGenerator(){
     aux = aux.toUpperCase()
     return 'n' + aux
 }
+
+
 document.getElementById("botao2").style.display="none"
 
 document.getElementById("botao1").onclick = function() {
@@ -36,5 +38,65 @@ document.getElementById("botao2").onclick = function() {
     document.getElementById("texto").innerHTML = bencaGenerator()
 }
 
+document.getElementById("img").onclick = function() {
+    this.src = "zc.jpeg"
+    document.getElementById("botao1").style.display = "none"
+    document.getElementById("botao2").style.display = "none"
+    document.getElementById("titulo").innerHTML = "Volte Sempre"
+    document.getElementById("texto").style.display = "none"
+    document.getElementById("titulo").style.animationDuration = "0.5s"    
+}
 
+document.getElementById("sunset").onclick = function(){
+    document.body.classList.toggle("animacao")
+    document.getElementById("img").classList.toggle("img_animation")
+    if (this.innerHTML == "normal edition"){
+        this.innerHTML = "sunset edition"
+    }
+    else {
+        this.innerHTML = "normal edition"
+    }
+}
+
+
+var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+    65: 'a',
+    66: 'b'
+  };
+
+var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+var konamiCodePosition = 0;
+
+function activateCheats(){
+    document.getElementById("titulo").innerHTML = "PUDIM" 
+    document.getElementById("img").src = "pd.jpeg"
+    
+}
+
+
+document.addEventListener('keydown', function(e) {
+    // get the value of the key code from the key map
+    var key = allowedKeys[e.keyCode];
+    // get the value of the required key from the konami code
+    var requiredKey = konamiCode[konamiCodePosition];
+  
+    // compare the key with the required key
+    if (key == requiredKey) {
+  
+      // move to the next key in the konami code sequence
+      konamiCodePosition++;
+  
+      // if the last key is reached, activate cheats
+      if (konamiCodePosition == konamiCode.length) {
+        activateCheats();
+        konamiCodePosition = 0;
+      }
+    } else {
+      konamiCodePosition = 0;
+    }
+  });
 
